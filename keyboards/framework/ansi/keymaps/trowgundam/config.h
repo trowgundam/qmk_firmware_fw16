f4d50f78631d15c931afb7bdc671407394027bfc
Jeffrey Geer <viriveri@gmail.com>
2024-08-30 07:48:55 -0500

    feat: Added personalized config for the Framework 16 ANSI Keyboard

---
+++ keyboards/framework/ansi/keymaps/trowgundam/config.h
@@ -0,0 +1,8 @@
+#define TAPPING_TERM 125
+
+#define ENABLE_RGB_MATRIX_SOLID_COLOR
+
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_DEFAULT_HUE 170                           // Sets the default hue value, if none has been set
+#define RGB_MATRIX_DEFAULT_SAT 255                           // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS // Sets the default brightness value, if none has been set
